255c12ff7ce5ae18d0c31e33839b789f7fec2bc2
Tri Hung <koikoidth12@gmail.com>
2023-07-26 07:15:09 +0700

    create template handlebars

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const path = require('path');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// template engine
+app.engine('hbs', handlebars.engine({
+    extname:'.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources/views'));
+console.log(path.join(__dirname,'resources/views'))
+
+app.get('/',(req, res) => {
+    res.render('home');
+})
+
+app.get('/news',(req, res) => {
+    res.render('news');
+})
+
+app.listen(port,()=>console.log(`Example app listening at http://localhost:${port}`))
+// routes
+// start web server
+// import library express to initialize server
